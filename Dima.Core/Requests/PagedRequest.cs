9053f24e19c890d0351761e16b5d4e8dbc5e0e5e
Lucas Fernandes Lopes Rodrigues <rodrigues14set@hotmail.com>
2024-06-18 15:41:14 -0300

    Adding Responses and Requests

---
+++ Dima.Core/Requests/PagedRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Dima.Core;
+
+public abstract class PagedRequest : Request
+{
+    public int PageNumber { get; set; } = Configuration.DefaultPageNumber;
+    public int PageSize { get; set; } = Configuration.DefaultPageSize;
+}
---
+++ Dima.Core/Requests/Request.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Dima.Core;
+
+public abstract class Request
+{
+    public string UserId { get; set; } = string.Empty;
+}
