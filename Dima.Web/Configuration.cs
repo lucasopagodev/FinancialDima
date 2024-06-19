5c3ae2a7ef8f49bc3972c1c5087b39182896540a
Lucas Fernandes Lopes Rodrigues <rodrigues14set@hotmail.com>
2024-06-19 17:51:49 -0300

    Add DarkMode and fonts

---
+++ Dima.Web/Configuration.cs
@@ -0,0 +1,39 @@
+﻿using MudBlazor;
+using MudBlazor.Utilities;
+
+namespace Dima.Web;
+
+public static class Configuration
+{
+    public static MudTheme Theme = new()
+    {
+        Typography = new Typography 
+        {
+            Default = new Default()
+            {
+                FontFamily = ["Raleway", "sans-serif"]
+            }
+        },
+        Palette = new PaletteLight
+        {
+            Primary = new MudColor("#1EFA2D"),
+            PrimaryContrastText = new MudColor("#000000"),
+            Secondary = Colors.LightGreen.Darken3,
+            Background = Colors.Grey.Lighten4,
+            AppbarBackground = new MudColor("#1EFA2D"),
+            AppbarText = Colors.Shades.Black,
+            TextPrimary = Colors.Shades.Black,
+            DrawerText = Colors.Shades.White,
+            DrawerBackground = Colors.Green.Darken4
+        },
+        PaletteDark = new PaletteDark
+        {
+            Primary = Colors.LightGreen.Accent3,
+            Secondary = Colors.LightGreen.Darken3,
+            // Background = Colors.LightGreen.Darken4,
+            AppbarBackground = Colors.LightGreen.Accent3,
+            AppbarText = Colors.Shades.Black,
+            PrimaryContrastText = new MudColor("#000000")
+        }
+    };
+}
