5631b5a2a8ca5a5d574a0b3f19e559fec517c887
Lucas Fernandes Lopes Rodrigues <rodrigues14set@hotmail.com>
2024-06-19 18:52:45 -0300

    Create authentication state provider interface

---
+++ Dima.Web/Security/ICookieAuthenticationStateProvider.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Dima.Web.Security;
+
+public interface ICookieAuthenticationStateProvider
+{
+    Task<bool> CheckedAuthenticationAsync();
+    Task<AuthenticationState> GetAuthenticationStateAsync();
+    void NotifyAuthenticationStateChanged();
+}
