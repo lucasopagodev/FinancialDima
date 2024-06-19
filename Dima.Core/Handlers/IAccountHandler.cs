f3ff75693f8caf6b75ba18e205799e1a95b28fd4
Lucas Fernandes Lopes Rodrigues <rodrigues14set@hotmail.com>
2024-06-19 18:45:51 -0300

    Add AccountHandler on Web project.

---
+++ Dima.Core/Handlers/IAccountHandler.cs
@@ -0,0 +1,11 @@
+﻿using Dima.Core.Requests.Account;
+using Dima.Core.Responses;
+
+namespace Dima.Core;
+
+public interface IAccountHandler
+{
+    Task<Response<string>> LoginAsync(LoginRequest request);
+    Task<Response<string>> RegisterAsync(RegisterRequest request);
+    Task LogoutAsync();
+}
--- Dima.Web/Program.cs
+++ Dima.Web/Program.cs
@@ -7,7 +7,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+
+builder.Services.AddHttpClient(Configuration.HttpClientName, opt => 
+{
+    opt.BaseAddress = new Uri(Configuration.BackendUrl);
+}).AddHttpMessageHandler<CookieHandler>();
 
 await builder.Build().RunAsync();
